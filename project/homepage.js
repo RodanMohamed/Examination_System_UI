5217a4b77d1d4765f06ca831d6483097f53093d4
Rodan <mohamedrody554@gmail.com>
2025-12-22 20:20:13 +0200

    test github

--- project/homepage.js
+++ project/homepage.js
@@ -1,4 +1,4 @@
-// Start Exam button logic
+
 document.getElementById("btn").addEventListener("click", function() {
     var currentUser = localStorage.getItem("currentUser");
     var errorPara = document.getElementById("startExamError");
@@ -11,18 +11,18 @@ document.getElementById("btn").addEventListener("click", function() {
     }
 });
 
-// Logout button logic
+
 var logoutBtn = document.getElementById("logoutBtn");
 
-// Show logout if user is logged in
+
 if (localStorage.getItem("currentUser")) {
     logoutBtn.style.display = "inline-block";
 }
 
 logoutBtn.addEventListener("click", function() {
-    // Remove logged-in user
+
     localStorage.removeItem("currentUser");
-    // Optionally redirect to login page
+
     window.location.href = "homepage.html";
 });
 
